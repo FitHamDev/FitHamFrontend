8fa58f5015ff5c944287f365b73aa9773f4ddf24
Thomas Bosmans <thomas.bosmans@student.ucll.be>
2025-06-24 14:22:38 +0200

    use correct repo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isGithubPages = process.env.GITHUB_ACTIONS || false;
-const repo = 'FitHamKalender';
+const repo = 'FitHamFrontend';
 
 module.exports = {
   output: 'export',
