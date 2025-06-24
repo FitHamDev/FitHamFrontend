5c6421f27e7ab71c1fea00a1ee0bfb38e8bdd437
Thomas Bosmans <thomas.bosmans@student.ucll.be>
2025-06-24 14:37:04 +0200

    fix background sponsors

--- components/carrousel/carrouselSponserItem.tsx
+++ components/carrousel/carrouselSponserItem.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from "next/router";
+
 type Props = {
     images: string[];
 }
@@ -5,10 +7,14 @@ type Props = {
 
 const CarrouselSponserItem: React.FC<Props> = ({ images }) => {
   const image = images[0];
+  const { basePath } = useRouter();
   return (
     <div className="relative h-[27rem] w-[55%] flex flex-col items-center justify-center rounded-lg shadow-lg overflow-hidden bg-white/70">
       <div className="absolute inset-0 bg-gradient-to-br from-blue-300/40 via-blue-500/60 to-blue-900/80 z-0"></div>
-      <div className="absolute inset-0 bg-[url('/carrousel_item_pattern.png')] bg-cover bg-center opacity-[8%] z-10"></div>
+      <div 
+        className="absolute inset-0 bg-cover bg-center opacity-[8%] z-10"
+        style={{ backgroundImage: `url('${basePath}/carrousel_item_pattern.png')` }}
+      ></div>
       <div className="absolute flex flex-col items-center justify-center h-full w-full z-50 text-center">
         <img
           src={image}
