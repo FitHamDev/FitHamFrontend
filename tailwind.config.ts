1aeae770a662fa266dac744b74a9ac7cfb2ddae5
Thomas Bosmans <thomas.bosmans@student.ucll.be>
2026-02-21 15:43:09 +0100

    add utils to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './utils/**/*.{js,ts,jsx,tsx}',
   ],
   safelist: [
     'max-w-7xl',
